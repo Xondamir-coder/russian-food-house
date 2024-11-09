bc83e373dfb057b4846d3e39a2bb795959e2a91f
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-11-09 12:57:23 +0500

    header done

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,7 +1,180 @@
 <template>
-	<header class="header">header</header>
+	<header class="header section-padding">
+		<input type="checkbox" class="header__checkbox" id="menu-toggle" />
+		<RouterLink to="/" class="header__logo">
+			<svg class="icon-logo">
+				<use href="@/assets/sprite.svg#logo"></use>
+			</svg>
+		</RouterLink>
+		<nav class="header__nav">
+			<ul class="header__list">
+				<li class="header__item text-dark-blue" v-for="link in links" :key="link.to">
+					<RouterLink :to="link.to">{{ link.text }}</RouterLink>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<label for="menu-toggle" class="header__burger bg-secondary">
+		<svg class="icon-menu">
+			<use href="@/assets/sprite.svg#menu"></use>
+		</svg>
+	</label>
+	<div class="header__menu bg-secondary">
+		<nav class="header__menu-nav">
+			<ul class="header__menu-list">
+				<li class="header__menu-item" v-for="link in links" :key="link.to">
+					<RouterLink :to="link.to">{{ link.text }}</RouterLink>
+				</li>
+			</ul>
+		</nav>
+	</div>
 </template>
 
-<script setup></script>
+<script setup>
+const links = [
+	{
+		to: '/catalog',
+		text: 'Каталог'
+	},
+	{
+		to: '/about',
+		text: 'О проекте'
+	},
+	{
+		to: '/news',
+		text: 'Новости'
+	},
+	{
+		to: '/services',
+		text: 'Сервисы'
+	},
+	{
+		to: '/contacts',
+		text: 'Контакты'
+	},
+	{
+		to: '/recipes',
+		text: 'Рецепты'
+	}
+];
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use 'sass:map';
+.header {
+	position: sticky;
+	top: 0;
+	z-index: 100;
+	display: flex;
+	align-items: center;
+
+	@include mix.respond('md') {
+		padding-top: 10px;
+		padding-bottom: 10px;
+	}
+	&:has(.header__checkbox:checked) ~ .header__menu {
+		transform: translateX(0);
+		transition-delay: 0s;
+	}
+	&:has(.header__checkbox:checked) ~ .header__menu .header__menu-item {
+		transform: translateX(0);
+		opacity: 1;
+	}
+	&__menu {
+		transform: translateX(-100%);
+		position: fixed;
+		inset: 0;
+		display: grid;
+		z-index: 101;
+		place-content: center;
+		transition: transform 0.5s;
+		transition-delay: 1s;
+		&-list {
+			display: flex;
+			flex-direction: column;
+			gap: 20px;
+		}
+		&-item {
+			color: #fff;
+			text-align: center;
+			font-size: 30px;
+			font-family: var.$font-secondary;
+			font-weight: 500;
+			letter-spacing: 0.02em;
+			transition-property: opacity, transform;
+			transition-duration: 0.5s;
+			@for $i from 1 through 6 {
+				&:nth-child(#{$i}) {
+					transition-delay: #{$i * 0.1}s;
+					opacity: 0;
+					@if ($i % 2 == 0) {
+						transform: translateX(-50px);
+					} @else {
+						transform: translateX(50px);
+					}
+				}
+			}
+		}
+	}
+	&__burger {
+		top: 10px;
+		right: 10px;
+		position: absolute;
+		cursor: pointer;
+		width: 48px;
+		height: 48px;
+		display: grid;
+		place-content: center;
+		border-radius: 50%;
+		z-index: 102;
+		@include mix.respond('md', min) {
+			display: none;
+		}
+	}
+	&__logo {
+		transition: opacity 0.3s;
+	}
+	&__checkbox {
+		display: none;
+		&:checked ~ .header__logo {
+			opacity: 0;
+		}
+	}
+	&__nav {
+		align-self: stretch;
+		margin-right: 5vw;
+		flex-grow: 1;
+		display: flex;
+		justify-content: center;
+		padding: 1.2rem 0;
+		position: relative;
+		@include mix.respond('md') {
+			display: none;
+		}
+		&::after {
+			content: '';
+			position: absolute;
+			left: 0;
+			bottom: 0;
+			background: linear-gradient(
+				to right,
+				rgba(180, 200, 220, 0),
+				rgba(180, 200, 220, 1),
+				rgba(180, 200, 220, 0)
+			);
+			width: 100%;
+			height: 1px;
+		}
+	}
+	&__list {
+		display: flex;
+		align-items: center;
+		gap: 2.7vw;
+		font-family: var.$font-secondary;
+	}
+	&__item {
+		font-weight: 500;
+		letter-spacing: 0.02em;
+	}
+}
+</style>
