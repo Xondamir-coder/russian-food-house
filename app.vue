c775a2a3ad0380eadc52b96618c7cab38ad45b16
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-11-12 00:17:12 +0500

    lenis

--- app.vue
+++ app.vue
@@ -5,8 +5,22 @@
 </template>
 
 <script setup>
+import Lenis from 'lenis';
 useHead({
 	title: 'Russian Food House'
+});
+
+onMounted(() => {
+	// Initialize Lenis
+	const lenis = new Lenis();
+
+	// Use requestAnimationFrame to continuously update the scroll
+	function raf(time) {
+		lenis.raf(time);
+		requestAnimationFrame(raf);
+	}
+
+	requestAnimationFrame(raf);
 });
 </script>
 
