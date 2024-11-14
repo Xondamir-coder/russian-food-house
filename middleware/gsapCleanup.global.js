08371337cc86bc9f84384da9cf8af531e70e8c20
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-11-14 20:45:53 +0500

    minor fixes

--- middleware/gsapCleanup.global.js
+++ middleware/gsapCleanup.global.js
@@ -1,5 +1,8 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to, from) => {
 	const { $gsap, $ScrollTrigger } = useNuxtApp();
+
+	if (to.path === from.path) return;
+
 	$ScrollTrigger.killAll();
 	$gsap.globalTimeline.kill();
 });
